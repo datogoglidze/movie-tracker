4ca88fe4b8b2c937c28e5da1957ee062047325e4
Schwifter <d.goglidze@sangu.edu.ge>
2025-04-28 23:15:05 +0400

    Add template endpoints

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends
-from sqlmodel import SQLModel, Field, create_engine, Session
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 
-database = "sqlite:///movies.sqlite"
-engine = create_engine(database)
+connect_args = {"check_same_thread": False}
+engine = create_engine(
+    "sqlite:///movies_db.sqlite",
+    echo=True,
+    connect_args=connect_args,
+)
 SQLModel.metadata.create_all(engine)
 
 
@@ -14,17 +18,45 @@ def get_session():
 app = FastAPI()
 
 
-class Movie(SQLModel, table=True):
-    __tablename__ = "movies"
-
-    id: str = Field(default=None, primary_key=True)
+class MovieBase(SQLModel):
     name: str
     year: int
-    note: str | None = None
+    note: str | None = Field(default=None)
 
 
-@app.get("/movies", response_model=Movie)
-def read_one(movie_id: str, session: Session = Depends(get_session)):
+class Movie(MovieBase, table=True):
+    __tablename__ = "movies"
+
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class MovieCreate(MovieBase):
+    pass
+
+
+class MoviePublic(MovieBase):
+    id: int
+
+
+@app.on_event("startup")
+def startup():
+    SQLModel.metadata.create_all(engine)
+
+
+@app.get("/movies", response_model=list[Movie])
+def read_all(
+    session: Session = Depends(get_session),
+):
+    movies = session.exec(select(Movie)).all()
+
+    return movies
+
+
+@app.get("/movies/{movie_id}", response_model=Movie)
+def read_one(
+    movie_id: str,
+    session: Session = Depends(get_session),
+):
     movie = session.get(Movie, movie_id)
 
     if not movie:
@@ -33,10 +65,28 @@ def read_one(movie_id: str, session: Session = Depends(get_session)):
     return movie
 
 
-@app.post("/movies", response_model=Movie)
-def create_one(movie: Movie, session: Session = Depends(get_session)):
-    session.add(movie)
+@app.post("/movies", response_model=MoviePublic)
+def create_one(
+    movie: MovieCreate,
+    session: Session = Depends(get_session),
+):
+    db_movie = Movie.model_validate(movie)
+    session.add(db_movie)
     session.commit()
-    session.refresh(movie)
+    session.refresh(db_movie)
 
-    return movie
+    return db_movie
+
+
+@app.delete("/movies/{movie_id}")
+def delete_one(
+    movie_id: str,
+    session: Session = Depends(get_session),
+):
+    movie = session.get(Movie, movie_id)
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+
+    session.delete(movie)
+    session.commit()
